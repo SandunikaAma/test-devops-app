8d218c9f5566521d153fbb9b07b2cef9078a54ff
Sandunika <amarasekarasandunika@gmail.com>
2026-06-23 21:56:45 +0530

    Implement CRUD Operations

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.route.js';
+import usersRoutes from '#routes/users.route.js';
 import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
@@ -40,4 +41,5 @@ app.get('/api', (req, res)=>{
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
 export default app;
